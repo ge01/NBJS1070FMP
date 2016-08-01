1c33ad25d3ec60e701a8665e55fec62f090a628f
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-01 13:10:23 -0500

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+/****************************************************
+*           Using Parameters with Functions         *
+****************************************************/
+function meatball(x) {
+  alert("i love " + x);
+}
+
+meatball("bacon");
+meatball("natalie");
+meatball("good will hunting");
